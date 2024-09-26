424514dc6ae03a8f4a1352a52c9718dc4ea12ad4
SuperAmdine <amdine.project.ahc@gmail.com>
2024-09-26 09:00:31 +0100

    first commit

---
+++ app/carousel-properties.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { EmblaOptionsType } from "embla-carousel";
+import Image from "next/image";
+const OPTIONS: EmblaOptionsType = { align: "center" };
+function CarouselProperties() {
+  return (
+    <div className="py-16">
+      <EmblaCarousel options={OPTIONS} />
+    </div>
+  );
+}
+
+export default CarouselProperties;
+type PropType = {
+  options?: EmblaOptionsType;
+};
+
+const residences = [
+  {
+    name: "Emirates Oasis Villas",
+    image: "/eov6.jpg",
+  },
+  {
+    name: "Al Wasl Village",
+    image: "/al-wasl.jpg",
+  },
+  {
+    name: "The Residences",
+    image: "/polo-villas.jpg",
+  },
+];
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {Array.from(Array(6).keys()).map((index) => (
+            <div className="embla__slide " key={index}>
+              <Image
+                src={residences[index % 3].image}
+                width={600}
+                height={400}
+                alt=""
+                className="mb-2 rounded"
+              />
+              <h3 className="">{residences[index % 3].name}</h3>
+              {/* <div className="embla__slide__number">{index + 1}</div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls"></div>
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,219 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import CarouselProperties from "./carousel-properties";
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col min-h-screen h-screen font-serif-1">
+      <header className=" gap-8 h-12 border-b w-full flex items-center flex-row mx-auto shadow-sm bg-white/90 fixed top-0 z-10">
+        <div className="max-w-screen-lg mx-auto flex flex-row w-full items-center justify-between">
+          <Image
+            className="dark:invert opacity-90"
+            src="/logo.png"
+            alt="Next.js logo"
+            width={160}
+            height={19}
+            priority
+          />
+          <span>hey</span>
         </div>
+      </header>
+      <main className="flex flex-col flex-grow items-center  ">
+        <Hero />
+        <HomeMessage />
+        <FeaturedProjects />
+        <PropertiesForSale />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className=" h-8  flex gap-6 flex-wrap items-center justify-center my-auto">
+        <span>h</span>
       </footer>
     </div>
   );
 }
+const HomeMessage = () => {
+  const refRoot = useRef();
+  const refH2One = useRef();
+  const refH2Two = useRef();
+  const refP2 = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    let ctx = gsap.context(() => {
+      gsap.from(refRoot.current, {
+        opacity: 0,
+        duration: 1,
+        y: 20,
+        scrollTrigger: {
+          trigger: refRoot.current,
+          start: "top bottom",
+          toggleActions: "restart none none reverse",
+        },
+        ease: "power4.inAndOut",
+      });
+      gsap.from(refH2One.current, {
+        opacity: 0,
+        // duration: 1,
+        xPercent: 120,
+        scrollTrigger: {
+          trigger: refRoot.current,
+          start: "top bottom",
+          toggleActions: "restart none none reverse",
+        },
+        ease: "power4.inAndOut",
+      });
+      gsap.from(refH2Two.current, {
+        opacity: 0,
+        // duration: 1,
+        xPercent: -120,
+        scrollTrigger: {
+          trigger: refRoot.current,
+          start: "top bottom",
+          toggleActions: "restart none none reverse",
+        },
+        ease: "power4.inAndOut",
+      });
+      gsap.from(refP2.current, {
+        opacity: 0,
+        // duration: 1,
+        y: -120,
+        scrollTrigger: {
+          trigger: refP2.current,
+          start: "top bottom",
+          toggleActions: "restart none none reverse",
+        },
+        ease: "power4.inAndOut",
+      });
+    }, refRoot);
+    return () => ctx.revert();
+  }, [refRoot, refH2One]);
+  return (
+    <div className="bg-color-1 color-1 w-full min-h-screen flex items-center justify-center text-center">
+      <div className="flex flex-col gap-16">
+        <p
+          className="font-sans text-xs uppercase leading-6 tracking-widest"
+          ref={refRoot}
+        >
+          Welcome to Al Habtoor Real Estate
+        </p>
+        <div>
+          <h2 className="text-8xl " ref={refH2One}>
+            Unmatched dedication.
+          </h2>
+          <h2 className="text-8xl " ref={refH2Two}>
+            Exceptional value.
+          </h2>
+        </div>
+        <p
+          className="max-w-sm mt-8 mx-auto font-sans color-1 text-xs uppercase leading-6 tracking-widest"
+          ref={refP2}
+        >
+          Al Habtoor Real Estate (AHRE) offers a diverse range of properties,
+          from modern offices to luxury villas. Since 1993, AHRE has provided
+          expert service across the UAE and beyond as part of the Al Habtoor
+          Group.
+        </p>
+      </div>
+    </div>
+  );
+};
+const FeaturedProjects = () => {
+  return (
+    <div className=" w-full py-16 min-h-96 bg-[#f5f3ef]">
+      <div className="max-w-screen-lg mx-auto ">
+        <h2 className="text-6xl  text-underlined">Featured Projects</h2>
+
+        <div className="flex flex-col gap-32 my-16">
+          <Property1 />
+          <Property2 />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PropertiesForSale = () => {
+  return (
+    <div className="  py-5 min-h-96">
+      <div className="max-w-screen-lg mx-auto w-full ">
+        <div className="pre-title text-xs my-10">Curated Properties</div>
+        <h2 className="text-6xl  text-underlined">Properties For Sale</h2>
+      </div>
+      <CarouselProperties />
+    </div>
+  );
+};
+
+const Property1 = () => {
+  return (
+    <div className="flex flex-row  justify-start ">
+      <div className="image-holder px-8">
+        <Image src="/aht_pool2b.jpg" width={600} height={400} alt="" />
+      </div>
+      <div className="text-holder w-1/3 gap-4 flex flex-col px-8  border-l border-l-black/10">
+        <h3 className="text-2xl">Al Habtoor Tower</h3>
+        <p className="text-sm font-sans">
+          As the latest addition to Al Habtoor City, Al Habtoor Tower offers 82
+          stories of exquisite, spacious apartment homes and all-inclusive
+          luxury amenities to those longing to live in opulence.
+        </p>
+        <Link className="text-left text-green-600 text-sm" href="/">
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+const Property2 = () => {
+  return (
+    <div className="flex flex-row-reverse   justify-start ">
+      <div className="image-holder px-8">
+        <Image src="/the-residence-grand.jpg" width={600} height={400} alt="" />
+      </div>
+      <div className="text-holder w-1/3 gap-4 flex flex-col px-8   file:border-r-black/10">
+        <h3 className="text-2xl">The Residences</h3>
+        <p className="text-sm font-sans">
+          Al Habtoor Group’s latest architectural masterpiece is nestled in one
+          of the most enchanting locations at Al Habtoor Grand Resort. This
+          extraordinary tower graces the Dubai Harbor skyline, offering
+          unparalleled access to the sea, airport, and public transportation
+          networks. As we stand on the threshold of luxury, we invite you to
+          explore this splendid addition to our prestigious portfolio.
+        </p>
+        <Link className="text-left text-green-600 text-sm" href="/">
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+const Hero = () => {
+  return (
+    <section className="hero w-full">
+      <video className="hero-video" autoPlay loop muted>
+        <source src="/home-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="hero-content bg-gradient-to-t from-[#141414] via-transparent to-transparent w-full h-full flex flex-col justify-end">
+        <div className="hero-gradient flex flex-col justify-end items-start p-8 gap-4">
+          <h1 className="hero-title font-light leading-none">
+            Exclusive Properties tailored for your needs
+          </h1>
+          <p className="hero-description">
+            Trusted name in selling, leasing, and management of commercial and
+            residential properties.
+          </p>
+          <Button className="ml-auto gap-4" variant={"secondary"} size={"lg"}>
+            Get Started <ArrowRightIcon size={16} />
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
